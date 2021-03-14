ce549488251b035ae0e3debc1376aceb6456fec4
Craig S. Keogh <craig.s.keogh@gmail.com>
2021-03-14 13:49:54 +1030

    Remove pageInfo hasNextPage, not needed for totals

--- src/app.ts
+++ src/app.ts
@@ -38,27 +38,54 @@ async function getContributions(login: string, startYear: number, endYear: numbe
         {
             user(login: "${login}") {
                 contributionsCollection(from: "${yearStr}-01-01T00:00:00", to: "${yearStr}-12-31T23:59:59") {
+                    commitContributionsByRepository(maxRepositories: 100) {
+                        repository {
+                            nameWithOwner
+                        }
+                        contributions(first: 10) {
+                            totalCount
+                        }
+                    }
                     issueContributionsByRepository(maxRepositories: 100) {
                         repository {
                             nameWithOwner
                         }
-                        contributions(first: 100) {
-                            pageInfo {
-                                hasNextPage
-                            }
+                        contributions(first: 10) {
                             totalCount
-                        }  
+                        }
+                    }
+                    pullRequestContributionsByRepository(maxRepositories: 100) {
+                        repository {
+                            nameWithOwner
+                        }
+                        contributions(first: 10) {
+                            totalCount
+                        }
+                    }
+                    pullRequestReviewContributionsByRepository(maxRepositories: 100) {
+                        repository {
+                            nameWithOwner
+                        }
+                        contributions(first: 10) {
+                            totalCount
+                        }
                     }
                 }
             }
           }`};
         const contrib: any = await doFetch(gql);
         process.stdout.write('.');
+        for (let repo of contrib.data.user.contributionsCollection.commitContributionsByRepository) {
+            file.write(`${yearStr},${login},${repo.repository.nameWithOwner},commits,${repo.contributions.totalCount}\n`);
+        }
         for (let repo of contrib.data.user.contributionsCollection.issueContributionsByRepository) {
-            if (repo.contributions.pageInfo.hasNextPage) {
-                throw `paging not implemented (${repo.repository.nameWithOwner})`
-            }
             file.write(`${yearStr},${login},${repo.repository.nameWithOwner},issues,${repo.contributions.totalCount}\n`);
+        }
+        for (let repo of contrib.data.user.contributionsCollection.pullRequestContributionsByRepository) {
+            file.write(`${yearStr},${login},${repo.repository.nameWithOwner},pr,${repo.contributions.totalCount}\n`);
+        }
+        for (let repo of contrib.data.user.contributionsCollection.pullRequestReviewContributionsByRepository) {
+            file.write(`${yearStr},${login},${repo.repository.nameWithOwner},review,${repo.contributions.totalCount}\n`);
         }
         await new Promise(resolve => setTimeout(resolve, 1000));
     }
